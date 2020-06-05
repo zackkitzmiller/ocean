2c3f158783b4405a4b3226ee0439c6a295cf6be1
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-06-05 04:31:20 +0100

    fixup: fix actually running on RPi

--- enable-service.sh
+++ enable-service.sh
@@ -1,4 +1,4 @@
 #1/bin/bash
 
-sudo cp etc/systemd/system/ocean.system /etc/systemd/system/ocean.service
+sudo cp etc/systemd/system/ocean.service /etc/systemd/system/ocean.service
 sudo systemctl enable ocean
